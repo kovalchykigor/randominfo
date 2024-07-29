4a1c1c881b45350bb58d368b26411b8b851a7faf
kovalchykigor <kovalchykigor@gmail.com>
2024-07-29 16:16:36 +0300

    home work 17 is done

---
+++ test.py
@@ -0,0 +1,10 @@
+import randominfo
+
+person = randominfo.Person()
+#print(person.full_name, person.gender, person.country, person.address)
+print(person.full_name)
+print(person.gender)
+print(person.country)
+print(person.address)
+# while person.country != "!Andhra Pradesh":
+#     print(person.country)
